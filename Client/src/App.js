c67c013cd5fc5e442dc27443b03f18e43783c8ef
Aayush Kumar <aayush@Aayushs-MacBook-Air.local>
2023-11-02 16:36:28 +0530

    homepage

---
+++ Client/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import React from 'react';
+import Navbar from './Components/Navbar';
+import Header from './Components/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Header />
+    </div>
+  );
+}
+
+export default App;
