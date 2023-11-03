45d5998de52cd47a700151dc3afa33a3d72ae5e0
Anand930singh <anand2002king@gmail.com>
2023-11-03 22:27:56 +0530

    reintialise the project

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,13 +1,8 @@
-import './App.css';
-import React from 'react';
-import Navbar from './Components/Navbar';
-import Header from './Components/Header';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Header />
+      hii
     </div>
   );
 }
