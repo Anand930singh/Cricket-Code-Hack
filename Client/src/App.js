85b10a5b5a3964b484ef2e03b2eb0cead7af5df1
Anand930singh <anand2002king@gmail.com>
2023-11-03 23:06:14 +0530

    added components

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,8 +1,15 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      hii
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home/>}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
