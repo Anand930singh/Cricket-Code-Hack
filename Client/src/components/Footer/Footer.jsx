e24910e5d71df8ef2601dbe0e32a05d063c52500
Anish-kumar7641 <72142986+Anish-kumar7641@users.noreply.github.com>
2023-11-05 00:25:03 +0530

    added navbar and footer

--- Client/src/components/Footer/Footer.jsx
+++ Client/src/components/Footer/Footer.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+
+import { BsFacebook, BsTwitter, BsInstagram, BsYoutube } from "react-icons/bs";
+import "./Footer.css";
+import React from "react"
 
 function Footer() {
   return (
-    <div>Footer</div>
+ 
+      <div className="bottom">
+        <ul className="social">
+          <li>
+            <BsFacebook size="40" />
+          </li>
+          <li>
+            <BsTwitter size="40" />
+          </li>
+          <li>
+            <BsInstagram size="40" />
+          </li>
+          <li>
+            <BsYoutube size="40" />
+          </li>
+        </ul>
+        <ul>
+          <li>About</li>
+          <li>Need Help?</li>
+          <li>Contact</li>
+          <li>Privacy</li>
+          <li>Terms of use</li>
+          <li>Advertising</li>
+          <li>Partners</li>
+        </ul>
+
+        <p>© CricketCodeHack. All rights reserved</p>
+      </div>
+    
   )
 }
 
--- Client/src/components/Navbar/Navbar.jsx
+++ Client/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import {Link,NavLink} from 'react-router-dom'
+import './Navbar.css'
+
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className="nav-bar">
+        <Link className="logo" to="/" >
+            Logo  
+        </Link>
+        <nav>
+            <NavLink exact="true" activeclassname="active" className="offers-link" to="/offers">
+              Offers
+            </NavLink>
+            <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
+                Contact
+            </NavLink>
+            <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
+                About
+            </NavLink>
+            <NavLink exact="true" activeclassname="active" className="features-link" to="/features">
+                Features
+            </NavLink>
+
+        </nav>
+    </div>
+     
+    
   )
 }
 
