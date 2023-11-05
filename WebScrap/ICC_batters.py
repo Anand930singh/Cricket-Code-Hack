e17a8a009903203d66431876a00b4d705c2bbbb8
Anand930singh <anand2002king@gmail.com>
2023-11-05 18:23:54 +0530

    modification in data

--- WebScrap/ICC_batters.py
+++ WebScrap/ICC_batters.py
@@ -25,15 +25,16 @@ def get_api_data(date, place):
         parsed_date = datetime.strptime(date, "%d %b %Y")
         formatted_date = parsed_date.strftime("%Y-%m-%d")
 
-        api_url = f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{place}/{formatted_date}/{formatted_date}?unitGroup=us&include=days&key=FQULW6FVZ26H9M9FE75BFGRCQ&contentType=json"
-        print(api_url)
+        # api_url = f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{place}/{formatted_date}/{formatted_date}?unitGroup=us&include=days&key=FQULW6FVZ26H9M9FE75BFGRCQ&contentType=json"   
+        api_url = f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{place}/{formatted_date}/{formatted_date}?unitGroup=us&include=days&key=ZDNKN86N22MS4H6KF9HHBBJ2F&contentType=json"
+        # print(api_url)
         response = requests.get(api_url)
         
         if response.status_code == 200:
                 wet = response.json()
                 if wet:
                     if 'days' in wet:
-                        return {"temp":wet['days'][0]['temp'], "hum":wet['days'][0]['humidity'],"wind":wet['days'][0]['windspeed']}
+                        return {"temp":wet['days'][0]['temp'], "hum":wet['days'][0]['humidity'],"wind":wet['days'][0]['windspeed'],"conditions":wet['days'][0]['conditions']}
         return None
 
 data = []
@@ -47,11 +48,9 @@ for url in urls:
     link = soup.find(class_='icc-home')
     # print(link.text)
     # print(link.text)
-    pattern = r'\b[A-Z][A-Za-z]+\s[A-Z][A-Za-z]+\b'
-
-    Player_name = re.search(pattern, link.text).group()
-
-    # print(Player_name)
+    # pattern = r'\b[A-Z][A-Za-z]+\s[A-Z][A-Za-z]+\b'
+    xyz=link.text
+    result_array = xyz.split(" / ")
 
     for table in engine_table:
         caption = table.find('caption')
@@ -68,10 +67,12 @@ for url in urls:
                     row_data.append('Player')
                     i = 1
                 else:
-                    row_data.append(Player_name)
+                    row_data.append(result_array[2])
 
                 date=''
                 place=''
+                format=''
+                opposition=''
                 for cell in cells:
                     if cell == cells[-1]:
                         continue
@@ -79,24 +80,39 @@ for url in urls:
                         date=cell.text
                     if cell == cells[9]:
                         place = cell.text
-                        # print(place)
+                    if cell == cells[8] and row== rows[0]:
+                        continue
+                    if cell == cells[8] and row!= rows[0]:
+                        # print(cell.text)
+                        test_count = cell.text.split(" v ")
+                        # print(test_count)
+                        format=test_count[0]
+                        opposition=test_count[1]
+                        continue
                     row_data.append(cell.text)
                 if row == rows[0]:
                     row_data.append('temp')
                     row_data.append('humidity')
                     row_data.append('windspeed')
+                    row_data.append('conditions')
+                    row_data.append('format')
+                    row_data.append('opposition')
                 else:
                     wether_data=get_api_data(date,place)
                     if wether_data:
                         row_data.append(wether_data.get("temp", "N/A"))
                         row_data.append(wether_data.get("hum", "N/A"))
                         row_data.append(wether_data.get("wind", "N/A"))
+                        row_data.append(wether_data.get("conditions", "N/A"))
                     else:
                         row_data.append("N/A")
                         row_data.append("N/A")
                         row_data.append("N/A")
+                        row_data.append("N/A")
+                    row_data.append(format)
+                    row_data.append(opposition)
                 
-                
+                print(row_data)
                 data.append(row_data)
 
 df = pd.DataFrame(data)
