cb0d745f70ea5dc59045c5eafb34f1c14b9f1e29
Aayush Kumar <aayush@Aayushs-MacBook-Air.local>
2023-11-07 00:59:33 +0530

    change in frontend

--- Client/src/pages/Home/Home.jsx
+++ Client/src/pages/Home/Home.jsx
@@ -1,25 +1,32 @@
 import React from 'react';
 import './Home.css';
 import RotatingCube from '../../components/RotatingCube/RotatingCube';
+import { ReactComponent as BgImage } from '../../Images/bg.svg'; // Import the SVG as a React component
 
 function Home() {
   return (
     <div className="home">
-    <div className="homeCard">
-      <div className="homeBody">
-        <div className="leftHomeBody">
-          <div className="homeTagLine">
-            <div className="line-one">STEP UP YOUR </div>
-            <div className="line-two"> <span>PREDICTION</span> WITH US</div>
+      <div className="homeCard">
+        <div className="homeBody">
+          <div className="leftHomeBody">
+            <div className="homeTagLine">
+              <div className="line-one">STEP UP YOUR </div>
+              <div className="line-two">
+                <span>PREDICTION</span> WITH US
+              </div>
+            </div>
+            <div className="homeButton">
+              <button>PREDICT</button>
+            </div>
           </div>
-          <div className="homeButton">
-            <button>PREDICT</button>
+          <div className="rightHomeBody">
+            <RotatingCube />
           </div>
         </div>
-        <div className="rightHomeBody">
-          <RotatingCube/>
+        <div className="bgImage">
+          <BgImage className='svg1'/>
+          <BgImage className='svg2'/>
         </div>
-      </div>
       </div>
     </div>
   );
